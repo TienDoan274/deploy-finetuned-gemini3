05e250208a9e4657b12d7b740e65899374dd1515
TienDoan274 <doannhattien4@gmail.com>
2025-05-18 22:34:13 +0700

    a

--- run-docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Build the Docker image
-docker build -t gemma3-inference .
-
-# Run the container
-# Replace inference.py with your script name if different
-docker run -it --name gemma3-container_phuc gemma3-inference
